414159970338cbe889c1346099d73403ce0bd67a
Will Aronow <waronow123@gmail.com>
2021-08-14 15:46:58 -0700

    added delete button and functionality

--- dom_manipulation/domevents/script.js
+++ dom_manipulation/domevents/script.js
@@ -2,6 +2,7 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var list = document.querySelectorAll("li");
+var deleteBtns = document.querySelectorAll(".deleteBtn")
 
 function inputLength() {
 	return input.value.length;
@@ -9,7 +10,10 @@ function inputLength() {
 
 function createListElement() {
 	var li = document.createElement("li");
+	var btn = document.createElement("button");
 	li.appendChild(document.createTextNode(input.value));
+	btn.appendChild(document.createTextNode("X"));
+	li.appendChild(btn);
 	ul.appendChild(li);
 	input.value = "";
 }
@@ -27,7 +31,11 @@ function addListAfterKeypress(event) {
 }
 
 function updateItem(event) {
-	console.log(event.srcElement.classList.toggle("done"));
+	event.srcElement.classList.toggle("done");
+}
+
+function deleteItem(event) {
+	event.srcElement.parentElement.remove();
 }
 
 button.addEventListener("click", addListAfterClick);
@@ -36,4 +44,8 @@ input.addEventListener("keypress", addListAfterKeypress);
 
 list.forEach(item => {
 	item.addEventListener("click", updateItem);
+})
+
+deleteBtns.forEach(btn => {
+	btn.addEventListener("click", deleteItem);
 })
