8cc9d0f507731778bc375c2f66cc00c7d1943933
Will Aronow <waronow123@gmail.com>
2021-08-15 16:29:46 -0700

    added generate random gradient

--- dom_manipulation/backgroundgenerator/script.js
+++ dom_manipulation/backgroundgenerator/script.js
@@ -2,12 +2,27 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var random = document.querySelector(".random");
 
 (function() {
 	setGradient();
 	css.textContent = body.style.background + ";";
 })()
-  
+
+function componentToHex(c) {
+	var hex = c.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+  }
+
+function randomColor() {
+	var color = "#"
+	for (let i = 0; i < 3; i++) {
+		var num = Math.floor(Math.random() * 255);
+		num = componentToHex(num);
+		color += num;
+	}
+	return color;
+}
 
 function setGradient() {
 	body.style.background = 
@@ -20,6 +35,16 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function randomGradient() {
+	var first = randomColor();
+	var second = randomColor();
+	color1.value = first;
+	color2.value = second;
+	setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+random.addEventListener("click", randomGradient);
