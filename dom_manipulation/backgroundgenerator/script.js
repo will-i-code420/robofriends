571f551272d18c157d12e0c4560c0c1324328cb1
Will Aronow <waronow123@gmail.com>
2021-08-14 20:38:16 -0700

    added iife to set current color and display

--- dom_manipulation/backgroundgenerator/script.js
+++ dom_manipulation/backgroundgenerator/script.js
@@ -3,6 +3,12 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+(function() {
+	setGradient();
+	css.textContent = body.style.background + ";";
+})()
+  
+
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
