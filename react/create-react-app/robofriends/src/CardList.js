bb9fc82691f166d07e53e6631143d2e76f791fc3
Will Aronow <waronow123@gmail.com>
2021-09-27 15:56:27 -0700

    refactored card list

--- react/create-react-app/robofriends/src/CardList.js
+++ react/create-react-app/robofriends/src/CardList.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-	const cardComponent = robots.map((robot, i) => {
-		return <Card key={i} id={robot[i].id} name={robot[i].name} email={robot[i].email} />;
-	});
-	return <div>{cardComponent}</div>;
+	return (
+		<div>
+			{robots.map((robot, i) => {
+				return <Card key={i} id={robot[i].id} name={robot[i].name} email={robot[i].email} />;
+			})}
+		</div>
+	);
 };
 
 export default CardList;
