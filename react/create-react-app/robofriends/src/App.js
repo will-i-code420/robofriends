00a4fa6d3c2c66583536a2e5cfc2caa112b3d204
Will Aronow <waronow123@gmail.com>
2021-09-29 16:06:37 -0700

    added font and styling to app

--- react/create-react-app/robofriends/src/App.js
+++ react/create-react-app/robofriends/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
@@ -20,7 +21,7 @@ class App extends Component {
 		});
 		return (
 			<div className="tc">
-				<h1>RoboFriends</h1>
+				<h1 className="f1">RoboFriends</h1>
 				<SearchBox searchInput={this.onSearchInput} />
 				<CardList robots={filteredRobots} />
 			</div>
