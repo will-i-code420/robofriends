043216d34dcbf957166e0d24d9ef2eba5cbe2de6
Will Aronow <waronow123@gmail.com>
2021-09-29 15:57:44 -0700

    linked searchbox to app

--- react/create-react-app/robofriends/src/App.js
+++ react/create-react-app/robofriends/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-	return (
-		<div className="tc">
-			<h1>RoboFriends</h1>
-			<SearchBox />
-			<CardList robots={robots} />
-		</div>
-	);
-};
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots,
+			searchField: ''
+		};
+	}
+	onSearchInput = (e) => {
+		this.setState({ searchField: e.target.value });
+	};
+	render() {
+		const filteredRobots = this.state.robots.filter((robot) => {
+			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+		});
+		return (
+			<div className="tc">
+				<h1>RoboFriends</h1>
+				<SearchBox searchInput={this.onSearchInput} />
+				<CardList robots={filteredRobots} />
+			</div>
+		);
+	}
+}
 
 export default App;
